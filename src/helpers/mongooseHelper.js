f90b1cfaf6d30c15e29575c55fd1f8c04d38fa1e
Stephan <20800277+StephanWithPH@users.noreply.github.com>
2022-10-21 18:08:00 +0200

    Update mongooseHelper.js

--- src/helpers/mongooseHelper.js
+++ src/helpers/mongooseHelper.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 let makeConnection = () => {
-  mongoose.connect('mongodb://localhost:27017/quizzer');
+  mongoose.connect('mongodb+srv://stephanwithph:stephanwithph123@quizzer.yy68h63.mongodb.net/?retryWrites=true&w=majority');
   let db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function () {
